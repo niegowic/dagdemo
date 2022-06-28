ff1b54cfb90ac084cde486150425af3be3f6496b
niegowic <60968600+niegowic@users.noreply.github.com>
2022-06-28 14:06:26 +0200

    Add files via upload

---
+++ ingest_site_b_tables_dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1),
+    'owner': 'Airflow',
+    'email': 'owner@test.com'
+}
+
+def process(p1):
+    print(p1)
+    return 'done'
+
+with DAG(dag_id='Ingest_Lehighvalley_tables', schedule_interval='*/10 * * * *', default_args=default_args, catchup=False) as dag:
+    
+    # Tasks dynamically generated 
+    tasks_1 = [BashOperator(task_id='Ingest_table_{0}'.format(t), bash_command='sleep 5'.format(t)) for t in  ["Losses","Shift"]]
+    tasks_2 = [BashOperator(task_id='Validate_ingestion_{0}'.format(t), bash_command='sleep 5'.format(t)) for t in  ["Losses","Shift"]]
+
+    task_4 = PythonOperator(task_id='Validate_ingestion', python_callable=process, op_args=['my super parameter'])
+
+    task_5 = BashOperator(task_id='Send_notification', bash_command='echo "pipeline done"')
+
+    tasks_1 >> task_4 >> tasks_2 >> task_5
---
+++ ingest_site_demo2_tables_dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2020, 1, 1),
+    'owner': 'Airflow',
+    'email': 'owner@test.com'
+}
+
+def process(p1):
+    print(p1)
+    return 'done'
+
+with DAG(dag_id='Ingest_SiteB_tables', schedule_interval='*/45 * * * *', default_args=default_args, catchup=False) as dag:
+    
+    # Tasks dynamically generated 
+    tasks_1 = [BashOperator(task_id='Ingest_table_{0}'.format(t), bash_command='sleep 1'.format(t)) for t in  range(40)]
+    tasks_2 = [BashOperator(task_id='Validate_ingestion_{0}'.format(t), bash_command='sleep 1'.format(t)) for t in  range(40)]
+
+    task_4 = PythonOperator(task_id='Validate_ingestion', python_callable=process, op_args=['my super parameter'])
+
+
+    tasks_1 >> task_4 >> tasks_2
